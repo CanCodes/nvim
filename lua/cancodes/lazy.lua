709e5c591a56a2de0b94b91aa791eae50e39b721
Can <32599057+CanCodes@users.noreply.github.com>
2023-12-27 23:30:12 +0300

    lsps

--- lua/cancodes/lazy.lua
+++ lua/cancodes/lazy.lua
@@ -20,6 +20,11 @@ plugins = {
     'github/copilot.vim',
     {'fatih/vim-go', run = ':GoUpdateBinaries'},
     'tpope/vim-fugitive',
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
 }
 
 require('lazy').setup(plugins)

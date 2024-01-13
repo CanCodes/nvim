810f7335cf2823ff6d51ce79a9cbe758c8db9f61
Can <32599057+CanCodes@users.noreply.github.com>
2024-01-13 10:27:30 +0300

    added fuzzy search

--- lua/cancodes/lazy.lua
+++ lua/cancodes/lazy.lua
@@ -1,34 +1,36 @@
 -- lazy.nvim Setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 plugins = {
     'marko-cerovac/material.nvim',
-    {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'},
+    { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
     'nvim-lualine/lualine.nvim',
     'nvim-tree/nvim-web-devicons',
     'github/copilot.vim',
-    {'fatih/vim-go', run = ':GoUpdateBinaries'},
+    { 'fatih/vim-go',                    run = ':GoUpdateBinaries' },
     'tpope/vim-fugitive',
-    {"folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" }}, 
+    { "folke/todo-comments.nvim",         dependencies = { "nvim-lua/plenary.nvim" } },
     -- LSP Plugins
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
+    { 'junegunn/fzf',                     run = function() vim.fn['fzf#install']() end },
+    { 'junegunn/fzf.vim' }
 }
 
 require('lazy').setup(plugins)

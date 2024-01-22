3c187c35b05c013431ffe56f7004d328f48a66dd
Can <32599057+CanCodes@users.noreply.github.com>
2024-01-22 16:37:44 +0300

    templ, htmx lsps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,39 +1,60 @@
 local cmp = require('cmp')
 local lsp_zero = require('lsp-zero')
-
+local lspconfig = require('lspconfig')
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.buffer_autoformat()
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.buffer_autoformat()
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  -- Replace the language servers listed here 
-  -- with the ones you want to install
-  ensure_installed = {
-      "tsserver",
-      "rust_analyzer",
-      "gopls",
-      "eslint",
-      "lua_ls",
-      "pyright",
-      "templ",
-      "clangd",
-      "cmake",
---      "java_language_server",
-      "jsonls",
-      "dockerls",
-  },
-  handlers = {
-      lsp_zero.default_setup,
-  },
+    -- Replace the language servers listed here
+    -- with the ones you want to install
+    ensure_installed = {
+        "tsserver",
+        "rust_analyzer",
+        "gopls",
+        "eslint",
+        "lua_ls",
+        "pyright",
+        "templ",
+        "clangd",
+        "cmake",
+        --      "java_language_server",
+        "jsonls",
+        "dockerls",
+        "tailwindcss",
+        "htmx",
+        "html",
+    },
+    handlers = {
+        lsp_zero.default_setup,
+    },
+})
+
+lspconfig.html.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "html", "templ" },
+})
+
+lspconfig.htmx.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "html", "templ" },
+})
+
+lspconfig.tailwindcss.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "aspnetcorerazor", "astro", "astro-markdown", "blade", "clojure", "django-html", "htmldjango", "edge", "eelixir", "elixir", "ejs", "erb", "eruby", "gohtml", "gohtmltmpl", "haml", "handlebars", "hbs", "html", "html-eex", "heex", "jade", "leaf", "liquid", "markdown", "mdx", "mustache", "njk", "nunjucks", "php", "razor", "slim", "twig", "css", "less", "postcss", "sass", "scss", "stylus", "sugarss", "javascript", "javascriptreact", "reason", "rescript", "typescript", "typescriptreact", "vue", "svelte", "templ" }
 })
 
 cmp.setup({
     mapping = cmp.mapping.preset.insert({
-        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
 
     }),
 
